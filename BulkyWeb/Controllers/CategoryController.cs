c6a30058fdbfe6de07ec37109185cef6b1baecde
Ravi162910 <ravip357090@gmail.com>
2026-03-06 11:52:35 +1300

    Added Edit & Delete + Noticifation through partial views - for WK2 Lab

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -27,7 +27,70 @@ namespace BulkyWeb.Controllers
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id) 
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            Category categoryobj = _db.Categories.Find(id);
+            if (categoryobj == null) 
+            {
+                return NotFound();
+            }
+            return View(categoryobj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Category obj) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryobj = _db.Categories.Find(id);
+            if (categoryobj == null)
+            {
+                return NotFound();
+            }
+            return View(categoryobj);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category deleted successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
